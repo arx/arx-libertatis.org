99a84dc82a51d5056dca48c6dc444b7292ba6a75
Daniel Scharrer <daniel@constexpr.org>
2012-10-30 18:41:22 +0100

    Fix text shadow

--- htdocs/contact.html.php
+++ htdocs/contact.html.php
@@ -50,7 +50,7 @@ $p->import('icons');
 	
 	<h2>Webmaster</h2>
 	
-	<p>
+	<p class="center">
 		This website is hosted and administered by
 		<span itemscope itemtype="http://schema.org/Person">
 			<a href="<? url('p:constexpr') ?>" itemprop="url"><span itemprop="name"><? echo $p->i_daniel ?></span></a>.
