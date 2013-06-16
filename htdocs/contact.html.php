6a6a3c019bfa672a52c4d97a54f1561c3b50e239
Daniel Scharrer <daniel@constexpr.org>
2013-06-16 02:29:48 +0200

    Add forum links to the contact page

--- htdocs/contact.html.php
+++ htdocs/contact.html.php
@@ -15,7 +15,7 @@ $p->import('icons');
 	
 	<p>
 		We mainly hang out in the <b><a href="<?= url('p:irc') ?>"><?= url('c:irc') ?></a></b> channel on the <a href="<?= url('p:freenode') ?>">Freenode</a> network.<br>
-		If you are new to irc, please see the <a href="<?= url('wiki:IRC_channel') ?>">wiki page</a>!<br>
+		If you are new to IRC, please see the <a href="<?= url('wiki:IRC_channel') ?>">wiki page</a>!<br>
 	</p>
 	
 	<p>
@@ -29,19 +29,27 @@ $p->import('icons');
 	<h3><?= $p->i_reddit ?></h3>
 	
 	<p>
-		If your are more comfortable with message boards, feel free to post to <a href="<?= url('p:subreddit') ?>">/r/ArxFatalis</a>.
+		If your are more comfortable with message boards, feel free to post to <b><a href="<?= url('p:subreddit') ?>">/r/ArxFatalis</a></b>.
 	</p>
 	
 	<h3>Forums</h3>
 	
 	<p>
-		We don't have our own forum, but you'll probably be able to find help on one of the various existing <a href="<?= url('wiki:FAQ#Are_there_any_Arx_Libertatis_discussion_forums.3F') ?>">Arx Fatalis forums</a>.
+		We don't have our own forum, but occasionally visit some of the existing <a href="<?= url('wiki:FAQ#Are_there_any_Arx_Libertatis_discussion_forums.3F') ?>">Arx Fatalis communities</a>:
 	</p>
+	<ul>
+		<li><a href="<?= url('p:forum_arx')   ?>"><?= $p->bethesda_icon ?> Official Forum</a></li>
+		<li><a href="<?= url('p:forum_ttlg')  ?>"><?= $p->ttlg_icon ?> TTLG Forum</a></li>
+		<li><a href="<?= url('p:forum_gog')   ?>"><?= $p->gog_icon ?> GOG.com Forum</a></li>
+		<li><a href="<?= url('p:forum_steam') ?>"><?= $p->steam_icon ?> Steam Users Forum</a></li>
+		<li><a href="<?= url('p:comm_steam')  ?>"><?= $p->steam_icon ?> Steam Community</a></li>
+		<li><a href="<?= url('p:group_steam') ?>"><?= $p->steam_icon ?> Steam Group</a></li>
+	</ul>
 	
 	<h3><?= $p->bug_icon ?> Bug Tracker</h3>
 	
 	<p>
-		If you have found a problem in Arx Libertatis, please <a href="<?= url('p:bugs') ?>">open a bug report</a>.
+		If you have found a problem in Arx Libertatis, please <b><a href="<?= url('p:bugs') ?>">open a bug report</a></b>.
 	</p>
 	
 </section>
