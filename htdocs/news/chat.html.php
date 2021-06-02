f65ba4bb59a89918b4b532ab08d159f003a6db54
Daniel Scharrer <daniel@constexpr.org>
2021-06-02 23:25:39 +0200

    news/chat: Fix typo

--- htdocs/news/chat.html.php
+++ htdocs/news/chat.html.php
@@ -10,7 +10,7 @@ $p->import('makenews');
 ?>
 
 <p>
-	We have decided to move the <a href="<?= url('p:contact') ?>">Arx Libertatis development chat IRC channel</a> to the <a href="<?= url('p:libera') ?>">Libera.Chat</a> network. You can join us using the <b><a href="<?= url('p:chat') ?>">Libera.Chat web client</a></b> in your browser by connecting to <b><a href="<?= url('p:irc') ?>">irc.libera.chat:6697</a></b> using a standalone <a href="https://en.wikipedia.org/wiki/Comparison_of_Internet_Relay_Chat_clients">IRC client</a> and joining the <b><a href="<?= url('p:irc') ?>"><?= url('c:irc') ?></a></b> channel. Alternatively, enter the <b><a href="<?= url('p:matrix_room') ?>"><?= url('c:matrix') ?></a></b> room in a <a href="<?= url('p:matrix') ?>">Matrix</a> client.
+	We have decided to move the <a href="<?= url('p:contact') ?>">Arx Libertatis development chat IRC channel</a> to the <a href="<?= url('p:libera') ?>">Libera.Chat</a> network. You can join us using the <b><a href="<?= url('p:chat') ?>">Libera.Chat web client</a></b> in your browser or by connecting to <b><a href="<?= url('p:irc') ?>">irc.libera.chat:6697</a></b> using a standalone <a href="https://en.wikipedia.org/wiki/Comparison_of_Internet_Relay_Chat_clients">IRC client</a> and joining the <b><a href="<?= url('p:irc') ?>"><?= url('c:irc') ?></a></b> channel. Alternatively, enter the <b><a href="<?= url('p:matrix_room') ?>"><?= url('c:matrix') ?></a></b> room in a <a href="<?= url('p:matrix') ?>">Matrix</a> client.
 </p>
 
 <?
