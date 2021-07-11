255a75bfbe2ad66ae0530ea61fd4967bfa21a4c1
Daniel Scharrer <daniel@constexpr.org>
2021-07-11 11:59:41 +0200

    newslist: Exclude non-live items

--- include/newslist.php
+++ include/newslist.php
@@ -12,6 +12,10 @@ $items = array_merge(
 	compile_all('releases/*', $options)
 );
 
+$items = array_filter($items, function ($item) {
+	return !isset($item->live) || $item->live;
+});
+
 usort($items, function ($a, $b) {
 	return $b->time - $a->time;
 });
