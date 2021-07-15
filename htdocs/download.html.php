90659d742d598fefc934ba2a74384400baf7bae0
Daniel Scharrer <daniel@constexpr.org>
2021-07-15 08:30:56 +0200

    download: Mageia package updated to 1.2

--- htdocs/download.html.php
+++ htdocs/download.html.php
@@ -265,7 +265,7 @@ $p->import('icons');
 			<td><span class="mageia icon"></span> Mageia</td>
 			<td>Core</td>
 			<td><a href="https://madb.mageia.org/package/show/release/cauldron/application/0/name/arx-libertatis">arx-libertatis</a></td>
-			<td class="bad">1.2-dev</td>
+			<td class="good">1.2</td>
 			<td class="good">distro</td>
 			<td><a href="<?= url('wiki:Linux_packages#Mageia') ?>">Instructions</a></td>
 		</tr>
